82d459df6d4e96928089039d05ff4952d07f1101
KALAIVANI011 <167755146+KALAIVANI011@users.noreply.github.com>
2025-05-02 20:59:44 +0530

    Add files via upload

---
+++ armstrng.java
@@ -0,0 +1,28 @@
+public class armstrng {
+    public static void main(String[] args) {
+        int n=153;
+        int sum=0;
+        int rem;
+        int temp=n;
+        int count=0;
+        for(int i=n;i>0;i=i/10)//getting the digits 
+        {
+            count++;
+        }
+        while(n>0)
+        {
+            rem=n%10;
+            int powers=(int) Math.pow(rem,count);
+            sum=sum+powers;
+            n=n/10;
+        }
+        if(temp==sum)
+        {
+            System.out.println("amstrong");
+        }
+        else 
+        {
+            System.out.println("not amstrong");  
+        }
+    }
+}
